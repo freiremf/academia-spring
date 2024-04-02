6277f88b9ca60fe00f20d4f05e0b734911d18761
Matheus Freire <matheusfreire.cbcc@gmail.com>
2024-04-02 19:32:08 -0300

    init: novo projeto de academia

---
+++ src/test/java/com/org/academiaspring/AcademiaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.academiaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
