d58a6f972311a1c1445a52754451d0d0c645e768
Matheus Freire <matheusfreire.cbcc@gmail.com>
2024-04-02 22:18:47 -0300

    feat: adicionando crud de alunos - servico e repositorio

---
+++ src/main/java/com/org/academiaspring/service/AlunoServico.java
@@ -0,0 +1,38 @@
+package com.org.academiaspring.service;
+
+import com.org.academiaspring.model.AlunoConverter;
+import com.org.academiaspring.model.AlunoDto;
+import com.org.academiaspring.repository.AlunoRepositorio;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlunoServico {
+    private final AlunoRepositorio alunoRepositorio;
+
+    public AlunoServico(AlunoRepositorio alunoRepositorio) {
+        this.alunoRepositorio = alunoRepositorio;
+    }
+
+    public Page<AlunoDto> buscarTodos(Pageable pageable) {
+        return new PageImpl<>(alunoRepositorio.findAll(pageable)
+                .stream()
+                .map(AlunoConverter::toAlunoDto)
+                .toList());
+    }
+
+    public AlunoDto salvar(AlunoDto alunoDto) {
+        return AlunoConverter.toAlunoDto(alunoRepositorio.save(AlunoConverter.toAluno(alunoDto)));
+    }
+
+    public AlunoDto buscarPorMatricula(String matricula) {
+        return AlunoConverter.toAlunoDto(alunoRepositorio.findByMatricula(matricula).orElseThrow(
+                () -> new RuntimeException("Aluno não encontrado")));
+    }
+
+    public void deletarPorMatricula(String matricula) {
+        alunoRepositorio.deleteByMatricula(matricula);
+    }
+}
