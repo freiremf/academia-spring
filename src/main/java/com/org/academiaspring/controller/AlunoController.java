28cf22b6fcf0d725e3b618c21acb326c5114ad14
Matheus Freire <matheusfreire.cbcc@gmail.com>
2024-04-06 12:13:52 -0300

    feat: adicionando buscas por filtro

--- src/main/java/com/org/academiaspring/controller/AlunoController.java
+++ src/main/java/com/org/academiaspring/controller/AlunoController.java
@@ -1,9 +1,12 @@
 package com.org.academiaspring.controller;
 
 import com.org.academiaspring.model.AlunoDto;
+import com.org.academiaspring.model.AlunoFiltroDto;
 import com.org.academiaspring.service.AlunoServico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,8 +25,14 @@ public class AlunoController implements AlunoApi {
 
     @Override
     @GetMapping
-    public ResponseEntity<Page<AlunoDto>> buscarAlunos(Pageable pageable) {
+    public ResponseEntity<Page<AlunoDto>> buscarAlunos(@PageableDefault(direction = Sort.Direction.ASC) Pageable pageable) {
         return ok(alunoServico.buscarTodos(pageable));
+    }
+
+    @Override
+    @GetMapping("/filtro")
+    public ResponseEntity<Page<AlunoDto>> buscarAlunosPorFiltro(AlunoFiltroDto filtro, @PageableDefault(direction = Sort.Direction.ASC) Pageable pageable){
+        return ok(alunoServico.buscarPorFiltro(filtro, pageable));
     }
 
     @Override
